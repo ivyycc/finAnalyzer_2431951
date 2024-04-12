c3b081464101e35de6544489d8bb6b5c7e5b5773
ivyycc <ivy.chepkwony@gmail.com>
2024-04-12 15:52:28 +0200

    fixed code

--- FinancialHealthAnalyzer.py
+++ FinancialHealthAnalyzer.py
@@ -27,48 +27,94 @@ class FinancialHealthAnalyzer:
     def total_expenses(self):
         return sum(transaction.amount for transaction in self.transactions if transaction.type == "Expense")
 
+    def convertedIncome(self): #added method
+        var1 = self.total_revenue()*20 #Converts revenue from dollars to rands
+        return(var1)
+
     def profit(self):
+        var1 = self.convertedIncome()  #calls convertedincome so profit is calculated in rands
+        var2 = self.total_expenses()
+        return(var1 - var2)
     #write code for this method
 
     def profit_margin(self):
+        var1 = self.profit()
+        var2 = self.convertedIncome() 
+        return(var1/var2)
     #write code for this method
     
     def average_transaction_amount(self):
+        var1 = self.profit()
+        return(var1/len(self.transactions))  #Comment3: len() Gets length of transactions data array as amount of transactions
     #write code for this method
 
     #Determines finalncial health and returns the corresponding string
     def financial_health(self):
         profit = self.profit()
         if profit >= 0:
-            return "Healthy"
-        elif -1000 >= profit < 0:
-            return "Warning"
+            return("Healthy")
+        elif profit<0 and profit >= -1000: #checks that ONLY values from -1 and -1000 are flagged as "warning" using logic operator 'and'
+            return("Warning")
         else:
-            return "Critical"
+            return("Critical")
 
 class TestFinancialHealthAnalyzer(unittest.TestCase):
     #Setup data allows for code to be tested without manually writing test transaction code for every test function. 
     #setUp transaction data and structure may be changed to include more test functions.
     def setUp(self):
+
+        #items were added to the array and some original values were changed 
         transactions_data = [
-            FinancialTransaction("2024-01-01", "Income", 1000),
-            FinancialTransaction("2024-01-02", "Expense", 500),
-            FinancialTransaction("2024-01-03", "Expense", 300),
-            FinancialTransaction("2024-01-04", "Income", 1500)
+            FinancialTransaction("2024-01-01", "Income", 600),
+            FinancialTransaction("2024-01-02", "Expense", 15000),
+            FinancialTransaction("2024-01-03", "Expense", 20000),
+            FinancialTransaction("2024-01-04", "Income", 1500),
+            FinancialTransaction("2024-01-05", "Income", 105),
+            FinancialTransaction("2024-01-06", "Expense", 10000)
         ]
         self.transactions = transactions_data
 
     #Test case example that returns total revenue. Inluded as a tutorial for basis of other test cases.
     def test_total_revenue(self):
         analyzer = FinancialHealthAnalyzer(self.transactions)
-        self.assertEqual(analyzer.total_revenue(), 2500)
+        self.assertEqual(analyzer.total_revenue(), 2205)
+
+    #added test for checking conversion method 
+    def test_convertedIncome(self):
+        analyzer = FinancialHealthAnalyzer(self.transactions)
+        self.assertEqual(analyzer.convertedIncome(),44100)
 
     def test_total_expenses(self):
+        analyzer = FinancialHealthAnalyzer(self.transactions)
+        self.assertEqual(analyzer.total_expenses(), 45000)
+
     #This code needs to be completed
     def test_profit(self):
+        analyzer = FinancialHealthAnalyzer(self.transactions)
+        self.assertEqual(analyzer.profit(), -900)
+        self.assertNotEqual(analyzer.profit(), 1700) #checking that conversion did happen with assertNotEqual
     #This code needs to be completed
 
+    #added test for checking average transaction method
+    def test_averagetransamount(self):
+        analyzer = FinancialHealthAnalyzer(self.transactions)
+        self.assertEqual(analyzer.average_transaction_amount(),-150)
+
+    #added test for checking profit margin method
+    def test_profitmargin(self):
+        analyzer = FinancialHealthAnalyzer(self.transactions)
+        self.assertAlmostEqual(analyzer.profit_margin(), -0.02, places=2)#checks only to 2dp that the profit margins are as expected
+
+    #added test for checking financial health method
+    def test_financialhealth(self):
+        analyzer = FinancialHealthAnalyzer(self.transactions)
+        self.assertEqual(analyzer.financial_health(), "Warning")
+        self.assertNotEqual(analyzer.financial_health(), "Healthy") #checks that the financial health is strictly 'warning' and not 'critical' or 'healthy'
+        self.assertNotEqual(analyzer.financial_health(), "Critical")
+
     #Additional testing methods might be required. test_total_revenue can be changed/expanded
+
+
 
 #Main function is where your code starts to run. Methods need to be compiled correctly before they can be called from main    
 if __name__ == '__main__':
@@ -81,9 +127,10 @@ if __name__ == '__main__':
         ]
     FinancialHealthAnalyzer.transactions = transactions_data
     analyzer = FinancialHealthAnalyzer(FinancialHealthAnalyzer.transactions)
-    print("Profit: ")
-    print("Profit margin: ")
-    print("Average transaction amount: ")
-    print("Financial health: ")
-    unittest.main()
-    
+
+    #added the calling of the respective methods so that the values are outputed as expected 
+    print("Profit: ", analyzer.profit())
+    print("Profit margin: " , analyzer.profit_margin())
+    print("Average transaction amount: " ,analyzer.average_transaction_amount())
+    print("Financial health: " , analyzer.financial_health())
+    unittest.main(verbosity=2) #outputs which unit tests were run and the outcome (ok or FAIL) for each one
